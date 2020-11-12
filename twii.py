b908eb3c857155fb2a0e1f7ccee32e848f876250
Sandeep N <60995458+sandeepN06@users.noreply.github.com>
2020-11-12 23:36:20 +0530

    Update twii.py

--- twii.py
+++ twii.py
@@ -2,8 +2,8 @@
 from twilio.rest import Client
 import requests
 import json
-account_sid = 'AC5db2bccf9335d687dba51777f877c0d5'
-auth_token = '120fc5d32fc04d6faa804e2677b976df'
+account_sid = '---------------------------------------'
+auth_token = '----------------------------------------'
 client = Client(account_sid, auth_token)
 r = requests.get('http://api.open-notify.org/astros.json')
 people = r.json()
@@ -11,8 +11,8 @@ number_iss = people['number']
 Message = 'Hi '+str(number_iss)
 #formulate the message that will be sent
 message = client.messages.create(
-    to="+917892800801",
-    from_="+13158183050",
+    to="---------------------",
+    from_="------------------",
     body=Message)
 print(message.sid)
 
