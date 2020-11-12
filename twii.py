0cdd7a026c4efdc97c53a5877ecffcd39c3059ed
Sandeep N <60995458+sandeepN06@users.noreply.github.com>
2020-11-12 18:20:27 +0530

    Update twii.py

--- twii.py
+++ twii.py
@@ -19,25 +19,7 @@ print(message.sid)
 
 
 
-# importing twilio
-# from twilio.rest import Client
-#
-# # Your Account Sid and Auth Token from twilio.com / console
-# account_sid = 'AC5db2bccf9335d687dba51777f877c0d5'
-# auth_token = '120fc5d32fc04d6faa804e2677b976df'
-#
-# client = Client(account_sid, auth_token)
-#
-# ''' Change the value of 'from' with the number
-# received from Twilio and the value of 'to'
-# with the number in which you want to send message.'''
-# message = client.messages.create(
-# 							from_='+13158183050',
-# 							body ='body',
-# 							to ='+13158183050'
-# 						)
-#
-# print(message.sid)
+
 
 
 
